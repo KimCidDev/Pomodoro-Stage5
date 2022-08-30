d4f8af460fac2c669dd41955c4c99f03e3fba888
Kim <cid.kim@gmail.com>
2022-08-30 08:24:08 -0700

    DOM variables every button

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const playButton = document.querySelector('.play').innerHTML
+const pauseButton = document.querySelector('.play').innerHTML
+const stopButton = document.querySelector('.play').innerHTML
+const setButton = document.querySelector('.play').innerHTML
+const volumeUpButton = document.querySelector('.play').innerHTML
+const volumeDownButton = document.querySelector('.play').innerHTML
+
