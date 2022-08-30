1b662dff02a280d5f01250803b94e40a0073ee7c
Kim <cid.kim@gmail.com>
2022-08-30 13:52:11 -0700

    1st round button functionality

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+// BUTTONS
+
 const playButton = document.querySelector('.play')
 const pauseButton = document.querySelector('.pause')
-const stopButton = document.querySelector('.play').innerHTML
-const setButton = document.querySelector('.play').innerHTML
+const stopButton = document.querySelector('.stop')
+const setButton = document.querySelector('.set')
 const volumeUpButton = document.querySelector('.play').innerHTML
 const volumeDownButton = document.querySelector('.play').innerHTML
+
+// CLOCK
+
+let minutes = document.querySelector('#minutes').innerHTML
+let seconds = document.querySelector('#seconds').textContent
+
+let userMinutes
+
+// INTERACTIONS
 
 function playTimer() {
   return playButton.addEventListener('click', function () {
     pauseButton.classList.remove('hide')
     playButton.classList.add('hide')
+
+    stopButton.classList.remove('hide')
+    setButton.classList.add('hide')
   })
 }
 
@@ -18,7 +32,28 @@ function pauseTimer() {
   return pauseButton.addEventListener('click', function () {
     playButton.classList.remove('hide')
     pauseButton.classList.add('hide')
+    setButton.classList.remove('hide')
   })
 }
 
 pauseTimer()
+
+function stopTime() {
+  return stopButton.addEventListener('click', function () {
+    playButton.classList.remove('hide')
+    pauseButton.classList.add('hide')
+    setButton.classList.remove('hide')
+  })
+}
+
+stopTime()
+
+function setTime() {
+  return setButton.addEventListener('click', function () {
+    newMinutes = prompt('Quantos minutos deseja?')
+
+    document.querySelector('#minutes').innerHTML = newMinutes
+  })
+}
+
+setTime()
