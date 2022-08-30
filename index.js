9d41997a9512d2d4e26178a538860dc0e0c99833
Kim <cid.kim@gmail.com>
2022-08-30 09:19:28 -0700

    play/pause button behavior

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const playButton = document.querySelector('.play').innerHTML
-const pauseButton = document.querySelector('.play').innerHTML
+const playButton = document.querySelector('.play')
+const pauseButton = document.querySelector('.pause')
 const stopButton = document.querySelector('.play').innerHTML
 const setButton = document.querySelector('.play').innerHTML
 const volumeUpButton = document.querySelector('.play').innerHTML
 const volumeDownButton = document.querySelector('.play').innerHTML
 
+function playTimer() {
+  return playButton.addEventListener('click', function () {
+    pauseButton.classList.remove('hide')
+    playButton.classList.add('hide')
+  })
+}
+
+playTimer()
+
+function pauseTimer() {
+  return pauseButton.addEventListener('click', function () {
+    playButton.classList.remove('hide')
+    pauseButton.classList.add('hide')
+  })
+}
+
+pauseTimer()
